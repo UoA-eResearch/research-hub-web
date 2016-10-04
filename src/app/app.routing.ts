984399efb3043245abb1d502926fe085a66699bb
Jamie Diprose <jamie.diprose@gmail.com>
2016-10-05 11:00:05 +1300

    Product lists multi screen size friendly.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,10 +5,12 @@ import {PoliciesComponent} from "./policies/policies.component";
 import {ServicesComponent} from "./services/services.component";
 import {DetailsComponent} from "./details/details.component";
 import {FiltersComponent} from "./filters/filters.component";
+import {HomeComponent} from "./home/home.component";
 import {ModuleWithProviders} from "@angular/core";
 
 const appRoutes:Routes = [
     {path: 'playlist', outlet: 'filters', component: FiltersComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'education', component: EducationComponent},
     {path: 'education/details/:id', component: DetailsComponent},
     {path: 'guides', component: GuidesComponent},
@@ -17,8 +19,8 @@ const appRoutes:Routes = [
     {path: 'policies/details/:id', component: DetailsComponent},
     {path: 'services', component: ServicesComponent},
     {path: 'services/details/:id', component: DetailsComponent},
-    {path: '', redirectTo: '/services', pathMatch: 'full' },
-    {path: '**', redirectTo: '/services', pathMatch: 'full'}
+    {path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 export const appRoutingProviders:any[] = [];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+import {OnInit} from "@angular/core";
+
+@Component({
+    templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+    services:any[];
+
+    constructor() {
+
+    }
+  
+    ngOnInit() {
+
+    }
+
+}
