b63b7f7e368c6dd89c784889acb5376984cde22e
Jamie Diprose <jamie.diprose@gmail.com>
2016-10-25 16:18:44 +1300

    Remove unneeded imports.

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,9 +1,8 @@
 import {Component, AfterViewInit} from "@angular/core";
-import {OnInit, OnDestroy} from "@angular/core";
+import {OnInit} from "@angular/core";
 import {SearchService} from "../app.search.service";
 import {DrupalService} from "../app.drupal.service";
 import {Observable} from "rxjs/Rx";
-import {Http} from "@angular/http";
 
 @Component({
   templateUrl: './services.component.html'
@@ -11,7 +10,7 @@ import {Http} from "@angular/http";
 export class ServicesComponent implements OnInit, AfterViewInit {
   services: Observable<Array<string>>;
 
-  constructor(private searchService:SearchService, private drupalService: DrupalService, private http: Http) {
+  constructor(private searchService:SearchService, private drupalService: DrupalService) {
 
   }
 
