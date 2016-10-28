c2b0be46184d1af70257f2bae30546b2c9acfc6e
Dr. Ayesha Hakim <a.hakim@auckland.ac.nz>
2016-10-28 13:55:58 +1300

    add headers

--- src/app/app.drupal.service.ts
+++ src/app/app.drupal.service.ts
@@ -1,13 +1,18 @@
 import {Injectable} from '@angular/core';
 import {Subject} from "rxjs/Rx";
-import {Http, URLSearchParams} from "@angular/http";
+import {Http, URLSearchParams, Headers} from "@angular/http";
 
 @Injectable()
 export class DrupalService {
   private rootUrl = "https://localhost:3027/";
   private thisUrl = "https://researchit.cer.auckland.ac.nz/api/content";
-
+  private config;
+  
   constructor(private http:Http) {
+  //let headers = new Headers();
+    //headers.set('Accept', 'application/json');
+   // headers.append('Content-Type', 'application/json');
+  //  this.http.defaults.headers.common['Accept']= 'application/json, text/plain';
   }
 
  search(category:string, searchChange:Subject<any>, debounceDuration = 400) {
@@ -32,60 +37,62 @@ export class DrupalService {
   }
 
   rawSearch(category:string, term:string, subcategories:any[]) {
-    var search = new URLSearchParams();
-
+    var dosearch = new URLSearchParams();
+      console.log(dosearch);
     if (term != undefined && term.trim() != "") {
-      search.set('q', term);
+      dosearch.set('search_string', term);
     }
     else if (subcategories != undefined) {
       for (let subcat of subcategories) {
         if (subcat.value != "" && subcat.value != undefined)
-          search.set(subcat.key, subcat.value);
+          dosearch.set(subcat.key, subcat.value);
       }
     }
-
-     return this.http
-      .get(this.rootUrl + category, {search})
+    let doheaders = new Headers();
+    doheaders.set('Accept', 'application/json');
+      return this.http
+      .get(this.thisUrl + "/" + category, {search:dosearch, headers:doheaders})
       .map((response) => response.json());
   }
   
   rawContentSearch(category:string, term:string, subcategories:any[]) {
-    var search = new URLSearchParams();
-
+     var dosearch = new URLSearchParams();
+      console.log(dosearch);
     if (term != undefined && term.trim() != "") {
-      search.set('q', term);
+      dosearch.set('search_string', term);
     }
     else if (subcategories != undefined) {
       for (let subcat of subcategories) {
         if (subcat.value != "" && subcat.value != undefined)
-          search.set(subcat.key, subcat.value);
+          dosearch.set(subcat.key, subcat.value);
       }
     }
 
-
-       return this.http
-      .get(this.thisUrl + "?type=" + category, {search})
+    let doheaders = new Headers();
+    doheaders.set('Accept', 'application/json');
+      return this.http
+      .get(this.thisUrl + "?type=" + category, {search:dosearch, headers:doheaders})
       .map((response) => response.json());
-      
    
   }
   
  rawdetailSearch(category:string, term:string, subcategories:any[]) {
-    var search = new URLSearchParams();
-
+    var dosearch = new URLSearchParams();
+      console.log(dosearch);
     if (term != undefined && term.trim() != "") {
-      search.set('q', term);
+      dosearch.set('search_string', term);
     }
     else if (subcategories != undefined) {
       for (let subcat of subcategories) {
         if (subcat.value != "" && subcat.value != undefined)
-          search.set(subcat.key, subcat.value);
+          dosearch.set(subcat.key, subcat.value);
       }
     }
 
-
-       return this.http
-      .get(this.thisUrl + "/" + category, {search})
+      let doheaders = new Headers();
+    doheaders.set('Accept', 'application/json');
+      return this.http
+      .get(this.thisUrl + "/" + category, {search:dosearch, headers:doheaders})
       .map((response) => response.json());
       
    
