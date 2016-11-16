729bb69174ff504cdb0a1bbba84c2c7655d8dce8
Jamie Diprose <jamie.diprose@gmail.com>
2016-11-17 11:36:34 +1300

    Change number of columns based on width of page.

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,4 @@
-import {Component, AfterViewInit} from "@angular/core";
+import {Component, AfterViewInit, ViewChild} from "@angular/core";
 import {OnInit} from "@angular/core";
 import {SearchService} from "../app.search.service";
 import {DrupalService} from "../app.drupal.service";
@@ -10,20 +10,27 @@ import {Router} from "@angular/router";
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit, AfterViewInit {
-  services: Observable<Array<string>>;
-  loading: boolean;
+  @ViewChild('productList') productList;
+  productWidth: number;
+  services:Observable<Array<string>>;
+  loading:boolean;
 
-  constructor(private searchService:SearchService, private drupalService: DrupalService) {
+  constructor(private searchService:SearchService, private drupalService:DrupalService) {
+    this.productWidth = 180;
+  }
 
+  getColClasses() {
+    let productsPerRow = Math.min(Math.floor(this.productList.nativeElement.offsetWidth / this.productWidth), 12);
+    let gridWidth = Math.ceil(12 / productsPerRow);
+    return "s" + gridWidth + " m" + gridWidth + " l" + gridWidth;
   }
 
   ngOnInit() {
     this.services = this.drupalService.contentsearch('service', this.searchService.searchChange);
   }
 
-  ngAfterViewInit()
-  {
-    window.scrollTo(0,0);
+  ngAfterViewInit() {
+    window.scrollTo(0, 0);
     this.searchService.findAll();
   }
 }
