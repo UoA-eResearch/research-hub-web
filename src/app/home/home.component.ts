d0f11d7b7e1fcb7dbcc0b8b555ac4853cbc7bc12
Jamie Diprose <jamie.diprose@gmail.com>
2016-11-23 15:12:52 +1300

    Add education cards to home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild, AfterViewInit} from "@angular/core";
 import {DrupalService} from "../app.drupal.service";
 import {Router} from "@angular/router";
+import {Observable} from "rxjs/Rx";
 declare var $:any;
 
 @Component({
@@ -10,27 +11,17 @@ declare var $:any;
 export class HomeComponent implements OnInit, AfterViewInit {
   @ViewChild('productList') productList;
   private productWidth:number = 250;
-  private researchLifeCycle = [
-    {name: "Plan & Design", id: "11", products: []},
-    {name: "Create Collect Capture", id: "12", products: []},
-    {name: "Analyse & Interpret", id: "14", products: []},
-    {name: "Publish & Report", id: "13", products: []},
-    {name: "Discover & Re-use", id: "16", products: []}];
+  private taxonomies = {research_lifecycle: "3", service_type: "2"};
+  private lifeCycleTerms:Observable<Array<string>>;
+  private serviceTypeTerms:Observable<Array<string>>;
+  private educationalProducts = [];
+  private allProducts = {};
 
-  private serviceTypes = [
-    {name: "Research Computing", id: "1", products: []},
-    {name: "Research Data", id: "2", products: []},
-    {name: "Analysis & Visualisation", id: "4", products: []},
-    {name: "Specialised Equipment", id: "19", products: []},
-    {name: "Consultation Services", id: "3", products: []},
-    {name: "Communication & Publishing", id: "25", products: []}];
-
-  constructor(private drupalService:DrupalService, private router: Router) {
+  constructor(private drupalService:DrupalService, private router:Router) {
 
   }
 
-  navigateToLifecycle(lifeCycle)
-  {
+  navigateToLifecycle(lifeCycle) {
     this.router.navigate(['lifecycle', lifeCycle.id]);
   }
 
@@ -45,33 +36,74 @@ export class HomeComponent implements OnInit, AfterViewInit {
     return Math.max(Math.min(Math.floor(numProducts), 12), 1);
   }
 
+  getProducts(taxonomyId)
+  {
+    if(!(taxonomyId in this.allProducts))
+      return [];
+
+    return this.allProducts[taxonomyId];
+  }
+
+  populateProducts(data)
+  {
+    for (let i = 0; i < data.length; i++) {
+      let termId = data[i]["id"];
+      this.drupalService.getProducts({taxonomyId: termId}).subscribe(
+        data => {
+          this.allProducts[termId] = data;
+        }
+      );
+    }
+  }
+
   ngOnInit() {
+    this.lifeCycleTerms = this.drupalService.getTaxonomy(this.taxonomies.research_lifecycle);
+    this.serviceTypeTerms = this.drupalService.getTaxonomy(this.taxonomies.service_type);
+
+    this.lifeCycleTerms.subscribe(
+      data => {
+        this.populateProducts(data);
+      }
+    );
+
+    this.serviceTypeTerms.subscribe(
+      data => {
+        this.populateProducts(data);
+      }
+    );
+
+    this.drupalService.getProducts({type: "education"}).subscribe(
+      data => {
+        console.log("education", data);
+        this.educationalProducts = data;
+      }
+    );
 
     $(document).ready(function () {
       $('.parallax').parallax();
     });
 
-    for (let i = 0; i < this.researchLifeCycle.length; i++) {
-      let researchLifeCycle = this.researchLifeCycle[i];
-      this.drupalService.getProducts(researchLifeCycle.id).subscribe(
-        data => {
-          researchLifeCycle.products = data;
-        },
-        err => console.error(err),
-        () => console.log('Finished getting lifecycle:', researchLifeCycle.name)
-      );
-    }
-
-    for (let i = 0; i < this.serviceTypes.length; i++) {
-      let serviceTypes = this.serviceTypes[i];
-      this.drupalService.getProducts(serviceTypes.id).subscribe(
-        data => {
-          serviceTypes.products = data;
-        },
-        err => console.error(err),
-        () => console.log('Finished getting services:', serviceTypes.name)
-      );
-    }
+    // for (let i = 0; i < this.researchLifeCycle.length; i++) {
+    //   let researchLifeCycle = this.researchLifeCycle[i];
+    //   this.drupalService.getProducts(researchLifeCycle.id).subscribe(
+    //     data => {
+    //       researchLifeCycle.products = data;
+    //     },
+    //     err => console.error(err),
+    //     () => console.log('Finished getting lifecycle:', researchLifeCycle.name)
+    //   );
+    // }
+    //
+    // for (let i = 0; i < this.serviceTypes.length; i++) {
+    //   let serviceTypes = this.serviceTypes[i];
+    //   // this.drupalService.getProducts(serviceTypes.id).subscribe(
+    //   //   data => {
+    //   //     serviceTypes.products = data;
+    //   //   },
+    //   //   err => console.error(err),
+    //   //   () => console.log('Finished getting services:', serviceTypes.name)
+    //   // );
+    // }
   }
 
   ngAfterViewInit() {
