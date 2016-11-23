ba6f13cbdf4d9bd9d883bd6ba452dcb75706c7e0
Jamie Diprose <jamie.diprose@gmail.com>
2016-11-23 17:57:12 +1300

    Update routes to enable specification of parameters for taxonomy types.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,18 @@ import {LifecycleComponent} from "./lifecycle/lifecycle.component";
 import {ShowcaseComponent} from "./showcase/showcase.component";
 import {ProductListComponent} from "./productList/productList.component";
 import {ProductDetailsComponent} from "./productDetails/productDetails.component";
+import {ComingSoonComponent} from "./comingSoon/comingSoon.component";
 
 const appRoutes:Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'guides', component: ComingSoonComponent},
+    {path: 'policies', component: ComingSoonComponent},
+    {path: 'showcase', component: ComingSoonComponent},
     {path: 'lifecycle', component: LifecycleComponent},
-    {path: 'lifecycle/:type', component: LifecycleComponent},
+    {path: 'lifecycle/:id', component: LifecycleComponent},
     {path: 'showcase', component: ShowcaseComponent},
     {path: 'productList/:type', component: ProductListComponent},
+    {path: 'productList/:type/:serviceTypeId', component: ProductListComponent},
     {path: 'productList/:type/productDetails/:id', component: ProductDetailsComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: '**', redirectTo: '/home', pathMatch: 'full'}
