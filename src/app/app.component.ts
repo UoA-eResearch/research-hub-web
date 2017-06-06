b5ebaac4fa7fbe60bb65085e5f1f7fa6a2519d8e
Jamie Diprose <jamie.diprose@gmail.com>
2017-06-06 16:21:00 +1200

    Track page views with analytics service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import {Component, HostListener, OnInit, ViewChild, ViewEncapsulation} from '@an
 import {Observable} from 'rxjs/Observable';
 import * as moment from 'moment';
 import {MdSidenav} from '@angular/material';
-import {NavigationStart, Router, Event} from '@angular/router';
+import {NavigationStart, Router, Event, NavigationEnd, ActivatedRoute} from '@angular/router';
 import {SharedDataService} from "./app.sharedData.service";
+import {AnalyticsService} from "./app.analytics.service";
+import {LocationStrategy} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -16,9 +18,26 @@ export class AppComponent implements OnInit {
   category = 'All';
   searchText = '';
 
-  constructor(private sharedDataService: SharedDataService, private router: Router) {
+  constructor(private sharedDataService: SharedDataService,
+              private router: Router,
+              private analyticsService: AnalyticsService) {
 
+    // Track page views
+    this.router.events.subscribe(
+      (event: Event) => {
+        if (event instanceof NavigationEnd) {
+          const urlAndParams = event.urlAfterRedirects.split('/');
+
+          if (urlAndParams.length > 1) {
+            const pageName = urlAndParams[1];
+            console.log('pageName: ', pageName);
+            analyticsService.trackPageView(pageName);
+          }
+        }
+      });
   }
+
+
 
   ngOnInit() {
     this.sharedDataService.titleChange.distinctUntilChanged().subscribe(title => {
