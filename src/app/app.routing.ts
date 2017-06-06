27523a9c45692e993eb12ca9619717401e38e0b6
Jamie Diprose <jamie.diprose@gmail.com>
2017-06-06 16:21:12 +1200

    Add details route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,11 +5,13 @@ import {HomeComponent} from './home/home.component';
 import {ResultsComponent} from './results/results.component';
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
 
 
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'results/:searchText', component: ResultsComponent},
+  {path: 'details/:id', component: ProductDetailsComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
