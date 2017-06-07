9e2d6229d6f2f8b7197d3644f9e6c373812f489d
Jamie Diprose <jamie.diprose@gmail.com>
2017-06-07 14:49:34 +1200

    Track page with analytics service

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,8 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {SharedDataService} from "../app.sharedData.service";
+import {SharedDataService} from '../app.sharedData.service';
+import {AnalyticsService} from '../app.analytics.service';
+import {Location} from '@angular/common';
+
 
 @Component({
   selector: 'app-about',
@@ -7,13 +10,16 @@ import {SharedDataService} from "../app.sharedData.service";
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  title = 'About';
 
-  constructor(private sharedDataService: SharedDataService) {
+  constructor(private sharedDataService: SharedDataService, private location: Location,
+              private analyticsService: AnalyticsService) {
 
   }
 
   ngOnInit() {
-    this.sharedDataService.setTitle("About", false);
+    this.sharedDataService.setTitle(this.title, false);
+    this.analyticsService.trackPageView(this.location.path(), this.title);
   }
 
 }
