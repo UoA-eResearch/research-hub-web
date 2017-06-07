774dc6c77c66c242fb59cdc42da6f641a3281377
Jamie Diprose <jamie.diprose@gmail.com>
2017-06-07 14:53:16 +1200

    Add name parameter to details route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,7 +11,7 @@ import {ProductDetailsComponent} from './product-details/product-details.compone
 const appRoutes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'results/:searchText', component: ResultsComponent},
-  {path: 'details/:id', component: ProductDetailsComponent},
+  {path: 'details/:id/:name', component: ProductDetailsComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
