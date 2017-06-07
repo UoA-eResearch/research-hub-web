8d30b7e2b8c6b08ae80557f5f72a7ceb8aaa2f44
Jamie Diprose <jamie.diprose@gmail.com>
2017-06-07 14:40:57 +1200

    Track page with analytics service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
 import {SearchService} from '../app.search.service';
+import {AnalyticsService} from '../app.analytics.service';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent implements OnInit, OnDestroy {
 
   searchText: string;
+  routeParamsSub: Subscription;
 
-  constructor(private location: Location, private searchService: SearchService) { }
+  constructor(private location: Location, private route: ActivatedRoute, private searchService: SearchService,
+              private analyticsService: AnalyticsService) {
+
+  }
 
   ngOnInit() {
     this.searchText = this.searchService.getSearchText();
+    this.routeParamsSub = this.route.params.subscribe(params => {
+      this.analyticsService.trackPageView(this.location.path(), params['name']);
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeParamsSub.unsubscribe();
   }
 
   back() {
