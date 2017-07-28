e9eac87d0427a03c832369d5ef883a8520b76e62
Jamie Diprose <jamie.diprose@gmail.com>
2017-07-28 16:48:14 +1200

    Use app-search-bar component Navigate to search component when users type in search box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import {Component, OnInit, ViewEncapsulation} from "@angular/core";
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from "@angular/core";
 import * as moment from "moment";
-import {AnalyticsService} from "./app.analytics.service";
 import {BreadcrumbService} from "ng2-breadcrumb/ng2-breadcrumb";
 import {SideNavComponent} from "./sidenav/sidenav-component";
 import {NavigationService} from "./navigation.service";
+import {SearchBarService} from "./search-bar/search-bar.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
 
 
 @Component({
@@ -12,7 +14,9 @@ import {NavigationService} from "./navigation.service";
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent extends SideNavComponent implements OnInit {
+export class AppComponent extends SideNavComponent implements OnInit, OnDestroy {
+
+  private searchTextChangeSub: Subscription;
 
   menuItems = [
     {name: 'Home', icon: 'home', href: '/home'},
@@ -23,13 +27,14 @@ export class AppComponent extends SideNavComponent implements OnInit {
   ];
 
   categories = [];
-  category = 'all';
+  category = 8;
   searchText = '';
 
-  constructor(private breadcrumbService: BreadcrumbService, private navigationService: NavigationService) {
+  constructor(private breadcrumbService: BreadcrumbService, private navigationService: NavigationService,
+              private searchBarService: SearchBarService, private router: Router) {
     super();
 
-    // Populate categories for search bar
+    // Populate categories for search-bar bar
     this.categories = navigationService.getCategory('/').categories;
 
     // Create friendly names for menu items in breadcrumbs
@@ -40,6 +45,19 @@ export class AppComponent extends SideNavComponent implements OnInit {
 
   ngOnInit() {
     this.updateSideNav();
+
+    this.searchTextChangeSub = this.searchBarService.searchTextChange.distinctUntilChanged().subscribe(searchText => {
+      console.log('searchTextChange', searchText, this.router.url);
+
+      // Navigate to the search page if the user types text in
+      if (this.router.url !== '/search' && searchText != null && searchText.trim() !== '') {
+        this.router.navigate(['/search']);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.searchTextChangeSub.unsubscribe();
   }
 
   getYear() {
