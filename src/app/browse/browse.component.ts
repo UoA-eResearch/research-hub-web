3c7676582142c0323cfd92366b1310dc3eb6155e
Jamie Diprose <jamie.diprose@gmail.com>
2017-07-28 16:35:54 +1200

    Show progress bar when loading data and connect to api endpoint (work in progress)

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,17 +1,20 @@
-import {Component, OnInit} from '@angular/core';
-import {NavigationService} from "../navigation.service";
+import {Component, OnInit} from "@angular/core";
+import {CategoryType, NavigationService} from "../navigation.service";
 import {ActivatedRoute} from "@angular/router";
+import {ApiService, SearchParams} from "../app.api.service";
 
 @Component({
-  selector: 'app-results',
+  selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.scss']
 })
 export class BrowseComponent implements OnInit {
 
-  categories = [];
+  private categories = [];
+  private results = [];
+  private loadingData = true;
 
-  constructor(private navigation: NavigationService, private route: ActivatedRoute) {
+  constructor(private navigation: NavigationService, private route: ActivatedRoute, private apiService: ApiService) {
 
   }
 
@@ -21,15 +24,34 @@ export class BrowseComponent implements OnInit {
       const category = this.navigation.getCategory(categoryId);
 
       if (!category.isLeaf()) {
+        this.loadingData = false;
+
         // Remove 'all' category when root category is requested
         let start = 0;
         if (categoryId === '/') {
           start = 1;
         }
 
+        this.results = [];
         this.categories = category.categories.slice(start);
       } else {
         this.categories = [];
+        this.loadingData = true;
+        const searchParams = new SearchParams();
+
+        if (category.type === CategoryType.Category) {
+          searchParams.categories.push(category.categoryId);
+        } else if (category.type === CategoryType.Subcategory) {
+          searchParams.categories.push(category.parent.categoryId);
+          searchParams.subcategories.push(category.categoryId);
+        }
+
+        this.apiService.getSearchResults(searchParams).subscribe(
+          data => {
+            this.loadingData = false;
+            this.results = data;
+          }
+        );
       }
     });
   }
