ce8f560df563ac391b0e783d79037722e3c713bd
Sina Masoud-Ansari <sina.masoud.ansari@gmail.com>
2017-07-31 16:43:08 +1200

    basic contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,14 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+
+const PHYSICAL_ADDRESS: string [] = ['Centre for eResearch', 'Building 302, Level 5, Room 585',
+  '23 Symonds Street', 'Auckland Central', 'Auckland 1010', 'New Zealand'];
+
+const POSTAL_ADDRESS: string [] = ['Centre for eResearch', 'The University of Auckland',
+  'Private Bag 92019', 'Auckland 1142', 'New Zealand'];
+
+const EMAIL_ADDRESS = 'eresearch-support@auckland.ac.nz';
+
+const CONTACT_PHONE = '+64 9 373 7599 ext 822231';
 
 
 @Component({
@@ -8,11 +18,15 @@ import {Component, OnInit} from "@angular/core";
 })
 export class ContactComponent implements OnInit {
 
-  constructor() {
+  physical_address: string[] = PHYSICAL_ADDRESS;
+  postal_address: string [] = POSTAL_ADDRESS;
+  email_address: string = EMAIL_ADDRESS;
+  contact_phone: string = CONTACT_PHONE;
 
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
+
 
 }
