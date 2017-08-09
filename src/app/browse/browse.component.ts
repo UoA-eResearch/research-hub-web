16084a454c594d644d31147b624e0bce8b111b07
sina <s.ansari@auckland.ac.nz>
2017-08-09 17:10:46 +1200

    browse tiles

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -17,6 +17,13 @@ export class BrowseComponent implements OnInit {
   private categories = [];
   private results = [];
 
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
+
   constructor(private navigation: NavigationService, private route: ActivatedRoute, private apiService: ApiService,
               private progressBarService: ProgressBarService) {
 
