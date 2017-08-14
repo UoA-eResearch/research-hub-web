e8f61e562045190aad57c3f427602ec9a8712c74
Jamie Diprose <jamie.diprose@gmail.com>
2017-08-14 16:51:05 +1200

    Separate browse and browse results

---
+++ src/app/browse-results/browse-results.component.ts
@@ -0,0 +1,77 @@
+import {Component, OnInit} from '@angular/core';
+import {MenuItem, MenuItemType, MenuService} from "../menu.service";
+import {ActivatedRoute} from "@angular/router";
+import {ApiService, ContentItemsSearchParams, SearchParams} from "../app.api.service";
+import {ProgressBarService} from "../app.progress-bar.service";
+import {SearchBarService} from "../search-bar/search-bar.service";
+import {ObservableMedia} from "@angular/flex-layout";
+
+@Component({
+  selector: 'app-browse-results',
+  templateUrl: './browse-results.component.html',
+  styleUrls: ['./browse-results.component.scss']
+})
+export class BrowseResultsComponent implements OnInit {
+
+  private results = [];
+
+  constructor(private menuService: MenuService, private route: ActivatedRoute, private apiService: ApiService,
+              private progressBarService: ProgressBarService, private searchBarService: SearchBarService, private media: ObservableMedia) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const categoryId = MenuService.getMenuItemId([params['contentTypeId'], params['subcategoryId']]);
+      const menuItem = this.menuService.getMenuItem(categoryId);
+
+      if (menuItem.isLeaf()) {
+        this.searchBarService.setCategory(menuItem.id); // When navigating within menu item, set search category to that item
+
+        switch (menuItem.type) {
+          case MenuItemType.Content:
+            this.loadContent(menuItem);
+            break;
+          case MenuItemType.Guide:
+            break;
+          case MenuItemType.Person:
+            this.loadPeople();
+            break;
+          case MenuItemType.Policy:
+            this.loadPolicies();
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  }
+
+  private loadPolicies() {
+    this.apiService.getPolicies(new SearchParams()).subscribe(
+      page => {
+        this.results = page.content;
+      }
+    );
+  }
+
+  private loadPeople() {
+    this.apiService.getPeople(new SearchParams()).subscribe(
+      page => {
+        this.progressBarService.setHidden();
+        this.results = page.content;
+      }
+    );
+  }
+
+  private loadContent(menuItem: MenuItem) {
+    const searchParams = new ContentItemsSearchParams();
+    searchParams.setContentTypes([menuItem.contentTypeId]);
+
+    this.apiService.getContentItems(searchParams).subscribe(
+      page => {
+        this.progressBarService.setHidden();
+        this.results = page.content;
+      }
+    );
+  }
+}
--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,9 +1,7 @@
-import {Component, OnInit, Input} from "@angular/core";
+import {Component, OnInit, Input, OnDestroy} from "@angular/core";
 import {MenuItem, MenuItemType, MenuService} from "../menu.service";
 import {ActivatedRoute} from "@angular/router";
 import {ApiService, ContentItemsSearchParams, SearchParams} from "../app.api.service";
-import {Person} from "../model/Person";
-import {Content} from "../model/Content";
 import {ProgressBarService} from "../app.progress-bar.service";
 import {SearchBarService} from "../search-bar/search-bar.service";
 import {Subscription} from 'rxjs/Subscription';
@@ -14,36 +12,28 @@ import {MediaChange, ObservableMedia} from '@angular/flex-layout';
   templateUrl: './browse.component.html',
   styleUrls: ['./browse.component.scss']
 })
-export class BrowseComponent implements OnInit {
+export class BrowseComponent implements OnInit, OnDestroy {
 
   private menuItems = [];
-  private results = [];
-
-  watcher: Subscription;
-  activeMediaQuery = '';
-  mediaSize = '';
+  mediaSub: Subscription;
 
   @Input()
   numCols = 4;
-
   teal = '#0294a5';
   navy = '#004059';
   orange = '#ff8300';
   tileColors = [this.teal, this.navy, this.orange];
 
-  constructor(private menuService: MenuService, private route: ActivatedRoute, private apiService: ApiService,
-              private progressBarService: ProgressBarService, private searchBarService: SearchBarService, media: ObservableMedia) {
-
-    this.watcher = media.subscribe((change: MediaChange) => {
-      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : '';
-      console.log(change.mqAlias);
-      this.SetNumCategoryColumns(change.mqAlias);
-
-    });
+  constructor(private menuService: MenuService, private route: ActivatedRoute, private progressBarService: ProgressBarService,
+              private searchBarService: SearchBarService, private media: ObservableMedia) {
   }
 
 
   ngOnInit() {
+    this.mediaSub = this.media.subscribe((change: MediaChange) => {
+      this.setNumCategoryColumns(change.mqAlias);
+    });
+
     this.route.params.subscribe(params => {
       const categoryId = MenuService.getMenuItemId([params['contentTypeId'], params['subcategoryId']]);
       const menuItem = this.menuService.getMenuItem(categoryId);
@@ -54,95 +44,42 @@ export class BrowseComponent implements OnInit {
         // Remove 'all' category when root category is requested
         let start = 0;
         if (categoryId === '/') {
-          //start = 1; // designs now required all category to show ...
+          start = 1;
           this.searchBarService.setCategory('all');
         }
-
-        this.results = [];
         this.menuItems = menuItem.menuItems.slice(start);
-      } else {
-        this.menuItems = [];
-        this.progressBarService.setVisible();
-        this.searchBarService.setCategory(menuItem.id); // When navigating within menu item, set search category to that item
-
-        switch (menuItem.type) {
-          case MenuItemType.Content:
-            this.loadContent(menuItem);
-            break;
-          case MenuItemType.Guide:
-            break;
-          case MenuItemType.Person:
-            this.loadPeople();
-            break;
-          case MenuItemType.Policy:
-            this.loadPolicies();
-            break;
-          default:
-            break;
-        }
       }
     });
-
   }
 
-  private loadPolicies() {
-
-    this.apiService.getPolicies(new SearchParams()).subscribe(
-      page => {
-        this.results = page.content;
-      }
-    );
+  ngOnDestroy() {
+    this.mediaSub.unsubscribe();
   }
 
-  private loadPeople() {
-    this.apiService.getPeople(new SearchParams()).subscribe(
-      page => {
-        this.progressBarService.setHidden();
-        this.results = page.content;
-      }
-    );
-  }
-
-  private loadContent(menuItem: MenuItem) {
-    const searchParams = new ContentItemsSearchParams();
-    searchParams.setContentTypes([menuItem.contentTypeId]);
-
-    this.apiService.getContentItems(searchParams).subscribe(
-      page => {
-        this.progressBarService.setHidden();
-        this.results = page.content;
-      }
-    );
-  }
-
-  SetNumCategoryColumns(mqAlias: string) {
-    this.mediaSize = mqAlias;
-    switch(mqAlias) {
+  setNumCategoryColumns(mqAlias: string) {
+    switch (mqAlias) {
       case 'xs':
         this.numCols = 2;
         break;
       case 'sm':
-        this.numCols = 3;
+        this.numCols = 2;
         break;
       case 'md':
-        this.numCols = 4;
+        this.numCols = 2;
         break;
       case 'lg':
-        this.numCols = 5;
+        this.numCols = 3;
         break;
       case 'xl':
-        this.numCols = 5;
+        this.numCols = 4;
         break;
       default:
-        this.numCols = 5;
+        this.numCols = 4;
         break;
     }
-    //console.log(mqAlias + ": "+this.numCols);
   }
 
   getTileColor(id: number): any {
-    return { 'background-color': this.tileColors[id % 3] };
+    return {'background-color': this.tileColors[id % 3]};
   }
-
-
 }
