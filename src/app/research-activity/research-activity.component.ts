64411b3f2e6fd446853707926064a96940ee19d5
Jamie Diprose <jamie.diprose@gmail.com>
2017-08-14 16:51:46 +1200

    Create a separate component for research activity

---
+++ src/app/research-activity/research-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-research-activity',
+  templateUrl: './research-activity.component.html',
+  styleUrls: ['./research-activity.component.scss']
+})
+export class ResearchActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
