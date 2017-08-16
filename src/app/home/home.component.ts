1af5626dc70f2ff3ecc2fc5aec17d5dcb6c316a1
Jamie Diprose <jamie.diprose@gmail.com>
2017-08-16 17:02:43 +1200

    Get random homepage images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../app.api.service";
 
 
 @Component({
@@ -9,13 +10,17 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
 
   coverImageUrl: string;
+  coverImages = ['20151005_Science Detail_001_1680x220_BW.jpg',
+    '483_Pacific_28Sep10_1680x220_BW.jpg',
+    '20130930_UoA_Details_225_1680x220_BW.jpg'];
 
-  constructor() {
+  constructor(private apiService: ApiService) {
 
   }
 
   ngOnInit() {
-    const coverImageNum = Math.floor((Math.random() * 3) + 1); // Generate a random number between 1 and 3 and use it to get a random cover image
-    this.coverImageUrl = 'assets/cover-' + coverImageNum + '.jpg';
+    const coverImageIndex = Math.floor(Math.random() * 3); // Generate a random number between 1 and 3 and
+    // use it to get a random cover image
+    this.coverImageUrl = this.apiService.getAssetUrl('page-elements/' + this.coverImages[coverImageIndex]);
   }
 }
