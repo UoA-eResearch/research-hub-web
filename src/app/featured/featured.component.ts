5476e1443ba707e90c9de238e2d3d0ec5e91d621
Jamie Diprose <jamie.diprose@gmail.com>
2017-08-17 10:39:25 +1200

    Hook featured component up to REST API

--- src/app/featured/featured.component.ts
+++ src/app/featured/featured.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService, ContentItemsSearchParams} from "../app.api.service";
+import {Content} from "../model/Content";
 
 @Component({
   selector: 'app-featured',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedComponent implements OnInit {
 
-  constructor() { }
+  content: Content;
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    const searchParams = new ContentItemsSearchParams();
+    searchParams.setSize(1); // we only want to find out the total number of content items so just return 1 element
+
+    this.apiService.getContentItems(searchParams).subscribe(
+      page => {
+        const totalContentItems = page.totalElements;
+        const contentItemIndex = Math.floor(Math.random() * (totalContentItems - 1)) + 1;
+        console.log('contentItemIndex', contentItemIndex);
+        this.apiService.getContentItem(contentItemIndex).subscribe(content => {
+          this.content = content;
+        });
+      }
+    );
   }
 
 }
