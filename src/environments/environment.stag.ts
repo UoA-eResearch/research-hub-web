a483e6a2f5425033bd76cc648bd0132665e88ced
Jamie Diprose <jamie.diprose@gmail.com>
2017-11-16 14:08:52 +1300

    Set production variable to true

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   shibbolethSessionUrl: 'https://research-hub.cer.auckland.ac.nz:8443/Shibboleth.sso/Session.json',
   apiUrl: 'https://research-hub.cer.auckland.ac.nz:8443/api/',
   analyticsCode: 'UA-77710107-3'
