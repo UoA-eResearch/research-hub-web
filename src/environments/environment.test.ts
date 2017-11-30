ed01944b3bbcc32765c5a8ecece128870389f1e2
Jamie Diprose <jamie.diprose@gmail.com>
2017-12-01 10:38:42 +1300

    Add new environments

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  shibbolethSessionUrl: 'https://test.research-hub.cer.auckland.ac.nz/Shibboleth.sso/Session.json',
+  apiUrl: 'https://test.research-hub.cer.auckland.ac.nz/api/',
+  analyticsCode: 'UA-77710107-3'
+};
