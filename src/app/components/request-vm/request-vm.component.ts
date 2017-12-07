a2f83701a69cddedc9d8d83570158dbe292bf29b
Jamie Diprose <jamie.diprose@gmail.com>
2017-12-07 13:02:23 +1300

    Display errors in error dialog Redirect to SSO and re-submit if session has expired

--- src/app/components/request-vm/request-vm.component.ts
+++ src/app/components/request-vm/request-vm.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {DateAdapter, NativeDateAdapter} from '@angular/material/core';
 import {ApiService} from 'app/services/api.service';
@@ -7,7 +7,12 @@ import {AuthService} from '../../services/auth.service';
 import {MatHorizontalStepper} from '@angular/material/stepper';
 import {AppComponentService} from '../../app.component.service';
 import {HttpErrorResponse} from '@angular/common/http';
+import {Location} from '@angular/common';
+import {MatDialog} from '@angular/material/dialog';
 import 'rxjs/add/operator/retryWhen';
+import {ErrorDialogComponent} from '../shared/error-dialog/error-dialog.component';
+import {ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
 
 
 @Component({
@@ -15,7 +20,7 @@ import 'rxjs/add/operator/retryWhen';
   templateUrl: './request-vm.component.html',
   styleUrls: ['./request-vm.component.scss']
 })
-export class RequestVmComponent implements OnInit {
+export class RequestVmComponent implements OnInit, OnDestroy {
 
   private static requestVmFormKey = 'requestVmForm';
 
@@ -29,6 +34,7 @@ export class RequestVmComponent implements OnInit {
   public content: Content;
   public submitting = false;
   public response: any;
+  private routeParamsSub: Subscription;
   public title = 'Request a Research Virtual Machine';
 
 
@@ -52,7 +58,8 @@ export class RequestVmComponent implements OnInit {
   }
 
   constructor(private formBuilder: FormBuilder, dateAdapter: DateAdapter<NativeDateAdapter>,
-              private apiService: ApiService, public authService: AuthService, private appComponentService: AppComponentService) {
+              private apiService: ApiService, public authService: AuthService, private appComponentService: AppComponentService,
+              public dialog: MatDialog, private location: Location, private route: ActivatedRoute) {
     dateAdapter.setLocale('en-GB');
   }
 
@@ -71,26 +78,52 @@ export class RequestVmComponent implements OnInit {
         this.appComponentService.setTitle(this.title);
       });
 
-    // this.loadForm();
+    this.routeParamsSub =
+      this.route.queryParams
+        .subscribe(params => {
+          const retry = params['retry'];
+
+          if (retry) {
+            this.loadRequest();
+            // this.submit();
+          } else {
+            this.clearRequest();
+          }
+        });
   }
 
-  // saveForm() {
-  //   localStorage.setItem(RequestVmComponent.requestVmFormKey, JSON.stringify(this.requestVmForm.getRawValue()));
-  // }
-  //
-  // loadForm() {
-  //   const item = localStorage.getItem(RequestVmComponent.requestVmFormKey);
-  //
-  //   if (item !== null) {
-  //     const value = JSON.parse(item);
-  //     console.log(RequestVmComponent.requestVmFormKey, value);
-  //     this.requestVmForm.setValue(value);
-  //   }
-  // }
-  //
-  // clearForm() {
-  //   localStorage.removeItem(RequestVmComponent.requestVmFormKey)
-  // }
+  ngOnDestroy() {
+    this.routeParamsSub.unsubscribe();
+  }
+
+  saveRequest() {
+    localStorage.setItem(RequestVmComponent.requestVmFormKey, JSON.stringify(this.requestVmForm.getRawValue()));
+  }
+
+  loadRequest() {
+    const item = localStorage.getItem(RequestVmComponent.requestVmFormKey);
+
+    if (item !== null) {
+      const value = JSON.parse(item);
+      console.log(RequestVmComponent.requestVmFormKey, value);
+      this.requestVmForm.setValue(value);
+    }
+  }
+
+  clearRequest() {
+    localStorage.removeItem(RequestVmComponent.requestVmFormKey)
+  }
+
+  showErrorDialog(title: string, message: string, closeButtonName: string, timeout: number) {
+    return this.dialog.open(ErrorDialogComponent, {
+      data: {
+        title: title,
+        message: message,
+        closeButtonName: closeButtonName,
+        timeout: timeout
+      }
+    });
+  }
 
   submit() {
     const isValid = this.requestVmForm.valid;
@@ -107,19 +140,27 @@ export class RequestVmComponent implements OnInit {
           (response) => {
             this.response = response;
             this.stepper.selectedIndex = 1; // Navigate to second step
-            // this.clearForm();
             // TODO: set Done step to completed so that a tick appears next to 'Done', doesn't work at the moment
           },
           (err: HttpErrorResponse) => {
             if (err.error instanceof Error) {
-              console.log(`A client-side or network error occurred: ${err.error.message}`);
+              this.submitting = false;
+              this.showErrorDialog(`${err.name}: ${err.status.toString()}`, err.message, 'Close', undefined);
             } else {
-              console.log(`Backend returned code ${err.status}, body was: ${err.error}`, err);
-
-              // if (err.status === 401) {
-              //   // this.saveForm();
-              //   this.authService.login();
-              // }
+              if (err.status === 401) {
+                this.submitting = false;
+                const dialogRef = this.showErrorDialog(
+                  'Session expired',
+                  'Redirecting to UoA Single Sign On...',
+                  'Login',
+                  5000
+                );
+                dialogRef.afterClosed().subscribe(result => {
+                  const url = this.location.path(false) + '?retry=true';
+                  this.saveRequest();
+                  this.authService.login(url);
+                });
+              }
             }
           });
     }
