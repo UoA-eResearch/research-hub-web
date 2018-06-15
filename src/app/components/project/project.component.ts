ed8f3752dfe01c30d1a629f5857dcb3c2d651b12
Sam Kavanagh <hganavak@gmail.com>
2018-06-15 13:31:03 +1200

    Pushing presentation version

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -118,7 +118,7 @@ export class ProjectComponent implements OnInit, OnDestroy {
   updateProjectResourceGroupAccess(grouperGroupId: string, uoaId: number) {
     this.cerApiService.updateProjectResourceGroupAccess(this.project.code, grouperGroupId, uoaId).subscribe(response => {
       console.log('Grouper response to add user request: ', response);
-      this.newUserInformation = '<h3>Success!</h3> User (UoA ID: <span>' + uoaId + '</span>) to group: <span>' + grouperGroupId + '</span><br />Access to the associated VM may take up to 20 minutes';
+      this.newUserInformation = '<h3>Success!</h3> User (UoA ID: <span>' + uoaId + '</span>) to group: <span>' + grouperGroupId + '</span><br />Access to the associated VM may take a few minutes';
     });
   }
 
