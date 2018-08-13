2d0df319c22fbb0bc58ae8b38afc350002fa8f50
Noel <noel.zeng@auckland.ac.nz>
2018-08-14 09:34:43 +1200

    remove unused function definition

--- src/app/components/search-results/search-filters/search-filters.service.ts
+++ src/app/components/search-results/search-filters/search-filters.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import {FormGroup, FormControl } from '@angular/forms';
-import { Subject } from 'rxjs';
 
 @Injectable()
 export class SearchFiltersService {
 
   readonly filtersForm : FormGroup;
-  readonly filtersVisibilityChange : Subject<any> = new Subject<any>();
 
   constructor() {
     this.filtersForm = new FormGroup({
@@ -15,9 +13,5 @@ export class SearchFiltersService {
       orgUnitTags: new FormControl([]),
       researchActivityIds: new FormControl()
     });
-  }
-
-  setSidenav(isOpen : boolean){
-
   }
 }
