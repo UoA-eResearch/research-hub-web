f1b0abf94328a542788349ef535e1c7aa282e965
Arron M <amcl080@aucklanduni.ac.nz>
2018-08-30 13:17:48 +1200

    only attempt env.js copy in docker-entrypoint.sh if file exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+ENV_FILE_PATH=/env.js
 
-exec cp /env.js /usr/share/nginx/www/assets/env.js
+if [ -e ENV_FILE_PATH ]; then
+  exec cp /env.js /usr/share/nginx/www/assets/env.js
+fi
+
 exec nginx -g 'daemon off;' "$@"
 
