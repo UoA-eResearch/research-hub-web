e8728f1b279428f97afabe9ae1a2e86cbb399373
Sam Kavanagh <hganavak@gmail.com>
2018-10-29 14:03:00 +1300

    Began working on cardview external-href bug again in a new branch. Old fix/branch (cardview-external-hrefs) was fundamentally incompatible with recent changes to master

---
+++ src/app/directives/search-result-link.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, ElementRef, Input, AfterViewInit, HostListener } from '@angular/core';
+import { ListItem } from 'app/model/ListItem';
+import { AnalyticsService } from 'app/services/analytics.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Directive({
+    selector: '[appSearchResultLink]'
+})
+export class SearchResultLinkDirective implements AfterViewInit {
+    @Input() item: ListItem;
+
+    constructor(private el: ElementRef, private analyticsService: AnalyticsService, private route: ActivatedRoute) {}
+
+    @HostListener('click') click() {
+       if (this.item.type === 'policy') {
+           this.analyticsService.trackPolicy(this.item.title, this.item.url);
+       }
+   }
+
+    ngAfterViewInit() {
+        if (this.item.type === 'policy') {
+            this.el.nativeElement.href = this.item.url;
+            this.el.nativeElement.target = '_blank';
+        } else {
+            this.el.nativeElement.href = '#/' + this.item.type + '/' + this.item.id.toString();
+        }
+    }
+}
