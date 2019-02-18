ceba223cb1a19bb5917bb5739c02e6d5c4f0d1cb
Noel <39939032+uoa-noel@users.noreply.github.com>
2019-02-19 12:21:28 +1300

    Add ability to run the Hub locally for development (#35)
    - You can now create a local custom environment file by creating a /src/environments/environment.custom.ts file.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ "$1" == "--local" ]; then
+  # If the local flag is passed, we run a version of the web project set up for development.
+  # Run an install to make sure new Node dependencies since last Docker image build are installed.
+  npm install
+  ./node_modules/@angular/cli/bin/ng serve --port 80 --host 0.0.0.0 --disable-host-check -c local
+  exit
+fi
+
 env_js_path=/env.js
 
 if [ -e $env_js_path ]; then
