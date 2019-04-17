38dbd87d8b5131f9d33e61ec156c3ca236a4fd46
Hganavak <hganavak@gmail.com>
2019-04-18 09:14:14 +1200

    Updated e2e test case's documentation

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,12 +9,19 @@ describe('ResearchHub\'s Basic Functionality', () => {
     page = new ResearchHubPage();
   });
 
+  /**
+   * Visits the home page and checks it contains the heading 'Welcome to the ResearchHub'.
+   */
   it('can display welcome message', () => {
     page.navigateTo(browser.baseUrl).then(() => {
       expect(page.getWelcomeMessage()).toEqual('Welcome to the ResearchHub');
     });
   });
 
+  /**
+   * Visits the home page -> Clicks one of the 'Category' tiles -> Checks that the search results page has
+   * been successfully navigated to by checking the presence of the search results page title 'Results'.
+   */
   it('can browse by category', () => {
     page.navigateTo(browser.baseUrl).then(() => {
       browser.driver.findElement(by.className('tile-text')).click().then(() => {
@@ -32,12 +39,19 @@ describe('ResearchHub\'s Search Functionality', () => {
     page = new ResearchHubPage();
   });
 
+  /**
+   * Directly navigates to the search results page and checks the presence of the search results page title 'Results'.
+   */
   it('can directly navigate to search results page', () => {
     page.navigateTo(browser.baseUrl + '/#/search').then(() => {
       expect(browser.driver.findElement(by.className('search-results-title')).getText()).toEqual('Results');
     });
   });
 
+  /**
+   * Navigates to the home page -> Types 'vm' in the home page search bar -> Checks that the search results page has
+   * been successfully navigated to by checking the presence of the search results page title 'Results'.
+   */
   it('displays search results after typing in homepage search bar', () => {
     page.navigateTo(browser.baseUrl).then(() => {
       browser.driver.findElement(by.css('input')).sendKeys('vm').then(() => {
@@ -49,8 +63,8 @@ describe('ResearchHub\'s Search Functionality', () => {
   });
 
   /**
-   * Visits the home page, types 'biblioinformatics' in the search bar, navigates to the first search result
-   * item, and checks that its title = 'BiblioInformatics'
+   * Visits the home page -> Types 'biblioinformatics' in the home page search bar -> Clicks the first search result
+   * item -> Checks that its title = 'BiblioInformatics'.
    */
   it('displays correct search results that can be navigated to', () => {
     page.navigateTo(browser.baseUrl).then(() => {
