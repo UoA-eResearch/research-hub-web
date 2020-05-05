0d0230ed79bcc5e7cbb0c590c7fff99231a6d03c
warrickct <warrickct@gmail.com>
2020-05-05 14:27:03 +1200

    Created a separate grid with people and headers.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,18 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+// export const environment = {
+//   production: false,
+//   shibbolethSessionUrl: '',
+//   researchHubApiUrl: '',
+//   cerApiUrl: '',
+//   analyticsCode: ''
+// };
+
 export const environment = {
   production: false,
-  shibbolethSessionUrl: '',
-  researchHubApiUrl: '',
-  cerApiUrl: '',
+  shibbolethSessionUrl: '/Session.json',
+  researchHubApiUrl: 'https://dev.research-hub.cer.auckland.ac.nz/api/',
+  cerApiUrl: 'https://dev.research-hub.cer.auckland.ac.nz/cer-api/',
   analyticsCode: ''
-};
+}
