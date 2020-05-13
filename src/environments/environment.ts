46515de2e856285174a48fbfe7d9a88039ae5904
warrickct <warrickct@gmail.com>
2020-05-13 19:05:39 +1200

    removed old environment configuration for local development.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,18 +3,11 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-// export const environment = {
-//   production: false,
-//   shibbolethSessionUrl: '',
-//   researchHubApiUrl: '',
-//   cerApiUrl: '',
-//   analyticsCode: ''
-// };
-
 export const environment = {
   production: false,
-  shibbolethSessionUrl: '/Session.json',
-  researchHubApiUrl: 'https://dev.research-hub.cer.auckland.ac.nz/api/',
-  cerApiUrl: 'https://dev.research-hub.cer.auckland.ac.nz/cer-api/',
+  shibbolethSessionUrl: '',
+  researchHubApiUrl: '',
+  cerApiUrl: '',
   analyticsCode: ''
-}
+};
+
